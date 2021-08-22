b46ae04735fe537ecb779357c33e0445ef8ef542
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-23 00:51:20 +0200

    Fixed db bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const db = require("./db/sensorsData")
 const port = 1410;
 
 //Middleware
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/cars", async (req, res) => {
+app.post("/sensorsData", async (req, res) => {
   try {
     const results = await db.createSensorsData(req.body);
     console.log("req", req);
@@ -29,13 +29,13 @@ app.post("/cars", async (req, res) => {
   }
 });
 
-app.get("/cars", async (req, res) => {
+app.get("/sensorsData", async (req, res) => {
   try {
-    const cars = await db.getAllSensorsData();
-    res.status(200).json({ cars });
+    const sensorsData = await db.getAllSensorsData();
+    res.status(200).json({ sensorsData });
   } catch (err) {
     console.log("Error GET: ", err);
   }
 });
 
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+app.listen(port,  () => console.log(`Server is running on port: ${port}`));
