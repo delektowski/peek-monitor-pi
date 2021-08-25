5b6845b211f893bcbe9c3564f8fac2a9f7aa6970
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-25 23:32:47 +0200

    add ejs set temp leng23

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", async (req, res) => {
   try {
     const sensorsData = await db.getAllSensorsData();
     console.log("sensorsData23", sensorsData)
-    res.render("index", { temperature: sensorsData[sensorsData.length -1].temperature, kokos: "kokos" });
+    res.render("index", { temperature: sensorsData[sensorsData.length -1].temperature, pressure: sensorsData[sensorsData.length -1].pressure, humidity: sensorsData[sensorsData.length -1].humidity });
   } catch (err) {
     console.log("Display site error23: ", err);
   }
