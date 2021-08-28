d99b1072339e31071b8a62867668f4d124cbce45
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-29 00:10:59 +0200

    Added date to banner

--- execShell.js
+++ execShell.js
@@ -2,8 +2,8 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 async function lsWithGrep() {
   try {
-    await exec("raspistill -o /home/pi/image-small.jpg -w 640 -h 480m");
-    await exec("convert -size 640x480 image.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:\"TEXT FOR THE BOTTOM\" -gravity south -composite image.jpg");
+    await exec("raspistill -o ./image.jpg -w 640 -h 480m");
+    await exec(`convert -size 640x480 image.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:${new Date()} -gravity south -composite image.jpg`);
 
 
   } catch (err) {
