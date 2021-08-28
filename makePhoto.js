1c24a9613e93477342b878baea7659f9a24b65f7
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-29 00:26:55 +0200

    Added make photo

---
+++ makePhoto.js
@@ -0,0 +1,16 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+async function makePhoto() {
+  try {
+    await exec("raspistill -o ./photo.jpg -w 640 -h 480m");
+    await exec(
+      `convert -size 640x480 photo.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:\"${new Date().toString()}\" -gravity south -composite photo.jpg`
+    );
+    console.log("Photo was made: ", new Date().toString());
+  } catch (err) {
+    console.error(err);
+  }
+}
+module.exports = {
+  makePhoto,
+};
--- nodeBme280.js
+++ nodeBme280.js
@@ -1,48 +1,45 @@
 const BME280 = require("bme280-sensor");
 const axios = require("axios");
+const { makePhoto } = require("./makePhoto");
 
 // The BME280 constructor options are optional.
-
 const options = {
-    i2cBusNo: 1, // defaults to 1
-    i2cAddress: BME280.BME280_DEFAULT_I2C_ADDRESS(), // defaults to 0x77
+  i2cBusNo: 1, // defaults to 1
+  i2cAddress: BME280.BME280_DEFAULT_I2C_ADDRESS(), // defaults to 0x77
 };
 
 const bme280 = new BME280(options);
 
 // Read BME280 sensor data
-
 const readSensorData = async () => {
-    try {
-        const data = await bme280.readSensorData();
-        let sensorData = {
-            temperature: +(data && data.temperature_C),
-            humidity: +(data && data.humidity),
-            pressure: +(data && data.pressure_hPa),
-            measurementDate: new Date(),
-        };
-        const createTable = await axios.post(
-            "http://192.168.191.239:1410/createTable"
-        );
-        const saveSensorsData = await axios.post(
-            "http://192.168.191.239:1410/sensorsData",
-            sensorData
-        );
-        console.log("saveSensorsData status", saveSensorsData.status);
-    } catch (err) {
-        console.log("Something went wrong: ", err);
-    }
+  try {
+    const data = await bme280.readSensorData();
+    let sensorData = {
+      temperature: +(data && data.temperature_C),
+      humidity: +(data && data.humidity),
+      pressure: +(data && data.pressure_hPa),
+      measurementDate: new Date(),
+    };
+    await axios.post("http://192.168.191.239:1410/createTable");
+    const saveSensorsData = await axios.post(
+      "http://192.168.191.239:1410/sensorsData",
+      sensorData
+    );
+    console.log("saveSensorsData status", saveSensorsData.status);
+    await makePhoto();
+  } catch (err) {
+    console.log("Something went wrong: ", err);
+  }
 };
 
 // Initialize the BME280 sensor
-
 const startSensor = () =>
-    bme280
-        .init()
-        .then(() => {
-            console.log("BME280 initialization succeeded");
-            setInterval(readSensorData, 60000);
-        })
-        .catch((err) => console.error(`BME280 initialization failed: ${err} `));
+  bme280
+    .init()
+    .then(() => {
+      console.log("BME280 initialization succeeded");
+      setInterval(readSensorData, 1800000);
+    })
+    .catch((err) => console.error(`BME280 initialization failed: ${err} `));
 
 module.exports = startSensor;
