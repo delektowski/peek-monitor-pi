f748fb45c0f518bcf4504ac48c3be07ffab6eae3
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-28 22:02:18 +0200

    Added fixed export

--- utils/createTableName.js
+++ utils/createTableName.js
@@ -1,7 +1,11 @@
-export function createTableName() {
+function createTableName() {
   const date = new Date()
     const day = date.getDate()
     const month = date.getMonth() + 1
   const year = date.getFullYear()
     return `${day}-${month}-${year}`
 }
+
+module.exports = {
+  createTableName
+};
