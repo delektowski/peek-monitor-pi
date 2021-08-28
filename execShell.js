2814af2371fcacc1cfb033900118ab7ce834aa5c
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-29 00:16:20 +0200

    Added String to date on banner

--- execShell.js
+++ execShell.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require("child_process").exec);
 async function lsWithGrep() {
   try {
     await exec("raspistill -o ./image.jpg -w 640 -h 480m");
-    await exec(`convert -size 640x480 image.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:${new Date().toUTCString()} -gravity south -composite image.jpg`);
+    await exec(`convert -size 640x480 image.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:${String(new Date().toUTCString())} -gravity south -composite image.jpg`);
 
 
   } catch (err) {
