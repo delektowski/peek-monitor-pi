22c7f067b570a266efcbc8da30c575e1c73ea1f6
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-28 23:55:49 +0200

    Added banner to image

--- execShell.js
+++ execShell.js
@@ -3,6 +3,8 @@ const exec = util.promisify(require("child_process").exec);
 async function lsWithGrep() {
   try {
     await exec("raspistill -o /home/pi/image-small.jpg -w 640 -h 480m");
+    await exec("convert -size 640x480 image.jpg -size 640x20 -font \"Courier\" -pointsize 12 -background \"#800000\" -fill black -gravity center  caption:\"TEXT FOR THE BOTTOM\" -gravity south -composite image.jpg");
+
 
   } catch (err) {
     console.error(err);
